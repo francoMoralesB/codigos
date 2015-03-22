d0bc20bd2a5f615183d0dd09edab1eb69a7cbf28
franco <fmorales.barcelo@gmail.com>
2015-03-21 22:38:59 -0300

    probandoooo

---
+++ archivo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    FILE *pf;
+    
+    pf = fopen("archivo.csv","w");
+    if(pf!=NULL){
+                 printf("\nEl archivo se creo correctamente");
+                 exit(3);
+                 }
+                 puts("\nQue no se pudo crear el archivo");
+                 
+                 return 0;
+    }
